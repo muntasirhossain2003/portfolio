957e3db597b887ca17c33621b15b1a2356f4a37e
Siyam-Bhuiyan <111516393+Siyam-Bhuiyan@users.noreply.github.com>
2025-09-21 04:24:09 +0600

    basic

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import VintageNewspaper from "./components/VintageNewspaper";
+import "./styles/vintage-newspaper.css";
+
+function App() {
+  return <VintageNewspaper />;
+}
+
+export default App;
